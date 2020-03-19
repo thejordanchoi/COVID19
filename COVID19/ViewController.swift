50c5883ccb9fcc94ea49b6aca855f58619f2f3ab
Jordan Choi <thejordanchoi@gmail.com>
2020-03-18 22:17:50 -0700

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by Jordan Choi on 3/18/20.
+//  Copyright © 2020 Jordan Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
